dbcc979f97423cb257e47c032f18324b3bb78542
Maggie S <Maggiesaldivia@gmail.com>
2021-02-22 22:42:01 -0800

    add winning array combos and startgame function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,29 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 const cellElements = document.querySelectorAll('[data-cell]')
 let circleTurn
+const board = document.getElementById('board')
 
-cellElements.forEach(cell => {
-  cell.addEventListener('click', handleClick, { once: true })
-})
+
+startGame()
+
+function startGame() {
+  circleTurn = false
+  cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+}
 
 function handleClick(e) {
   const cell = e.target
@@ -15,6 +33,7 @@ function handleClick(e) {
   //checkforDraw
   //switchTurns
   swapTurns()
+  setBoardHoverClass()
 }
 
 function placeMark(cell, currentClass) {
@@ -23,4 +42,14 @@ function placeMark(cell, currentClass) {
 
 function swapTurns() {
   circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS)
+  } else {
+    board.classList.add(X_CLASS)
+  }
 }
